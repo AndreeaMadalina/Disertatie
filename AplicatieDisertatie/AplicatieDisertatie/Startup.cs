69c24dc8a1fb72b3ccf9687b16c1dd37204a8600
Andreea <andreea.madalinavulpe@gmail.com>
2018-05-16 19:34:53 +0300

    Started master application.

---
+++ AplicatieDisertatie/AplicatieDisertatie/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AplicatieDisertatie.Startup))]
+namespace AplicatieDisertatie
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
