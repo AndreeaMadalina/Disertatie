9c8d7b42c50e5fbd3b684d0867256fa7ef028873
Andreea <andreea.madalinavulpe@gmail.com>
2018-05-27 13:39:37 +0300

    Implemented the repository and unity of work patterns.

---
+++ AplicatieDisertatie/AplicatieDisertatie/DAL/IGenericRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AplicatieDisertatie.DAL
+{
+	public interface IGenericRepository<T> : IDisposable
+	{
+		IEnumerable<T> Get();
+		T GetByID(object id);
+		void Insert(T student);
+		void Delete(object id);
+		void Update(T student);
+		void Save();
+	}
+}
---
+++ AplicatieDisertatie/AplicatieDisertatie/DAL/UnitOfWork.cs
@@ -0,0 +1,52 @@
+﻿using AplicatieDisertatie.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AplicatieDisertatie.DAL
+{
+	public class UnitOfWork : IDisposable
+	{
+		private Entities _context = new Entities();
+		private GenericRepository<AspNetUser> _userRepository;
+		private GenericRepository<AspNetRole> _roleRepository;
+		private GenericRepository<AspNetUserClaim> _userClaimRepository;
+		private GenericRepository<AspNetUserLogin> _userLoginRepository;
+
+
+		public GenericRepository<AspNetUser> UserRepository => _userRepository ?? new GenericRepository<AspNetUser>(_context);
+
+		public GenericRepository<AspNetRole> RoleRepository => _roleRepository ?? new GenericRepository<AspNetRole>(_context);
+
+		public GenericRepository<AspNetUserClaim> UserClaimRepository => _userClaimRepository ?? new GenericRepository<AspNetUserClaim>(_context);
+
+		public GenericRepository<AspNetUserLogin> UserLoginRepository => _userLoginRepository ?? new GenericRepository<AspNetUserLogin>(_context);
+
+
+		public void Save()
+		{
+			_context.SaveChanges();
+		}
+
+		private bool disposed = false;
+
+		protected virtual void Dispose(bool disposing)
+		{
+			if (!this.disposed)
+			{
+				if (disposing)
+				{
+					_context.Dispose();
+				}
+			}
+			this.disposed = true;
+		}
+
+		public void Dispose()
+		{
+			Dispose(true);
+			GC.SuppressFinalize(this);
+		}
+	}
+}
